a31bb42e71ca777d69c6ea0b2a1d47fd15e35c1b
Poorvi <poorvi.b26@gmail.com>
2025-03-18 19:37:05 -0700

    add functions to send and receive messages

--- socket_client.py
+++ socket_client.py
@@ -1,36 +1,97 @@
 import socket
 import pygame
+from threading import Thread, Lock
+import random
+import json
+
 pygame.init()
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_address = ('142.58.219.181', 53444)  # server IP and port
+server_address = ('0.0.0.0', 53444)  # server IP and port
 # to get the server IP (same as client IP since running on local host, check print output of server program)
+client_id = random.randint(1000, 9999)  # unique client ID
 
 # Setting up display
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 BOARD_SIZE = min(SCREEN_WIDTH, SCREEN_HEIGHT) - 100
-pygame.display.set_caption("Whiteboard")
-
-
+pygame.display.set_caption(f"Deny and Conquer: Player {client_id}")
 
 message = "Hello UDP Server"
 client_socket.sendto(message.encode(), server_address)
     
-#Color definations 
+# Color definitions 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
-#Setting up the grid size
+PLAYER_COLORS = [
+    (255, 0, 0),    # Red
+    (0, 0, 255),    # Blue
+    (0, 255, 0),    # Green
+    (255, 255, 0),  # Yellow
+]
+my_color = None
+
+# Setting up the grid size
 GRID_SIZE = 8
 SQUARE_SIZE = BOARD_SIZE // GRID_SIZE
 GRID_TOP_LEFT_X = (SCREEN_WIDTH - BOARD_SIZE) // 2
 GRID_TOP_LEFT_Y = (SCREEN_HEIGHT - BOARD_SIZE) // 2
+PEN_THICKNESS = 5
 
-#Background
+# game state variables, updated when receiving messages from server
+game_state = {} # which player owns which square
+drawing_state = {} # which squares are being drawn on
+player_count = 0 # no. of players playing
+pixel_count = 0 # no. of pixels colored
+color_idx = 0
+font = pygame.font.SysFont(None, 26)
+
+# send message to the server
+def send_message(message_type, data=None):
+    # format and send message to server
+    message = {
+        "type": message_type,
+        "client_id": client_id,
+        "data": data or {}
+    }
+    message_str = json.dumps(message)
+    client_socket.sendto(message_str.encode(), server_address)
+
+# receive messages from the server
+def receive_message():
+    while True:
+        data, _ = client_socket.recvfrom(4096)
+        message = json.loads(data.decode())
+        if message["type"] == "start":
+            my_color = PLAYER_COLORS[message["data"]["color_idx"]]
+            player_count = message["data"]["player_count"]
+            print(f"Connected as player {client_id} with color {my_color}")
+            print(f"Number of players playing the game: {player_count}")
+        
+        # current game state, which player owns which square ...
+        elif message["type"] == "game_state":
+            game_state = message["data"]["game_board"]
+            player_count = message["data"]["player_count"]
+            drawing_state = message["data"].get("drawing", {})
+            
+        # updated when a player owns a square by coloring it more than 50%
+        elif message["type"] == "square_owned":
+            row = message["data"]["row"]
+            col = message["data"]["col"]
+            owner_id = message["data"]["owner_id"]
+            color_idx = message["data"]["color_idx"]
+            game_state[str(row)][str(col)] = {
+                "owner": owner_id,
+                "color_idx": color_idx
+            }
+        
+        # more message types go here ...
+
+# Background
 screen.fill(WHITE)
 
-#Drawing 8 by 8 Grid
+# Drawing 8 by 8 Grid
 for row in range(GRID_SIZE):
     for col in range(GRID_SIZE):
         x = GRID_TOP_LEFT_X + col * SQUARE_SIZE
@@ -50,12 +111,18 @@ for row in range(GRID_SIZE):
 # Update the display
 pygame.display.flip()
 
+# send message to server to join the game
+send_message("join")
+
 # Game loop (keeps the game running)
 running = True
+clock = pygame.time.Clock()
 while running:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+send_message("leave")
 pygame.quit()
 client_socket.close()
